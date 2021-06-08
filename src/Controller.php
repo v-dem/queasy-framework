b25e27e621f0c4167a1d5e14288ba556368b3a83
v-dem <vitaly_demyanenko@yahoo.com>
2021-06-08 10:10:47 +0300

    added Controller::view() experimental method

--- src/Controller.php
+++ src/Controller.php
@@ -15,5 +15,19 @@ class Controller
         $this->app = $app;
         $this->request = $request;
     }
+
+    protected function view($__page, array $__data = array(), $__responseCode = 200)
+    {
+        extract($__data);
+
+        ob_start();
+
+        require($__page);
+
+        $__body = ob_get_contents();
+        ob_end_clean();
+
+        return $this->app->response->withBody($__body)->withResponseCode($__responseCode);
+    }
 }
 
