d0bc3fd1e9d058be8a2735e03408797a52a2c462
v-dem <vitaly_demyanenko@yahoo.com>
2021-09-23 09:27:57 +0300

    fix code style issue

--- src/Controller.php
+++ src/Controller.php
@@ -33,15 +33,17 @@ class Controller
 
         ob_start();
 
-        require(isset($this->app->config['viewsPath'])
+        require isset($this->app->config['viewsPath'])
             ? $this->app->config['viewsPath'] . $__page
-            : $__page);
+            : $__page;
 
         $__body = ob_get_contents();
 
         ob_end_clean();
 
-        return $this->app->response->withBody(new Stream($__body))->withStatus($__responseCode);
+        return $this->app->response
+            ->withBody(new Stream($__body))
+            ->withStatus($__responseCode);
     }
 }
 
