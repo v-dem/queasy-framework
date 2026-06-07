021b38417e8534b53301a1e8516e0440e4a482b9
Vitaly Demyanenko <vitaly_demyanenko@yahoo.com>
2026-06-07 11:51:46 +0300

    preparing for release

---
+++ src/MiddlewareQueueProcessor.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace queasy\framework;
+
+use queasy\container\ServiceContainer;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+use Psr\Http\Server\RequestHandlerInterface;
+
+class MiddlewareQueueProcessor implements RequestHandlerInterface
+{
+    private $middlewares;
+
+    private $handler;
+
+    public function __construct(array $middlewares = array(), Closure $handler)
+    {
+        $this->middlewares = $middlewares;
+        $this->handler = $handler;
+    }
+
+    public function handle(ServerRequestInterface $request)
+    {
+        if (empty($this->middlewares)) {
+            return $this->handler($request);
+        }
+
+        $middleware = array_shift($this->middlewares);
+
+        return $middleware->process($request, $this);
+    }
+}
+
